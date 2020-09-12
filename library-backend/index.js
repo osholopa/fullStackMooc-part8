380c8c5c1789113bba2fc8aee016a83c93079b88
Oskari Holopainen <oskariholopa@gmail.com>
2020-09-12 10:10:06 +0300

    Revert "8.6: mutation addBook"
    This reverts commit 3c9bffb0dcfcc118251497dd8ea10ddb8c037514.

--- library-backend/index.js
+++ library-backend/index.js
@@ -1,5 +1,4 @@
 const { ApolloServer, gql } = require("apollo-server");
-const { v1: uuid } = require("uuid");
 
 let authors = [
   {
@@ -106,15 +105,6 @@ const typeDefs = gql`
     allBooks(author: String, genre: String): [Book!]
     allAuthors: [Author!]!
   }
-
-  type Mutation {
-    addBook(
-      title: String!
-      published: Int!
-      author: String!
-      genres: [String!]!
-    ): Book
-  }
 `;
 
 const resolvers = {
@@ -133,19 +123,6 @@ const resolvers = {
   Author: {
     bookCount: (root) => {
       return books.filter((book) => book.author === root.name).length;
-    },
-  },
-  Mutation: {
-    addBook: (root, args) => {
-      const book = { ...args, id: uuid() };
-      books = books.concat(book);
-      console.log(authors.find((a) => a.name === args.author))
-      if (!authors.find((a) => a.name === args.author)) {
-        console.log('adding new author')
-        const author = { name: args.author, id: uuid() };
-        authors = authors.concat(author);
-      }
-      return book;
     },
   },
 };
